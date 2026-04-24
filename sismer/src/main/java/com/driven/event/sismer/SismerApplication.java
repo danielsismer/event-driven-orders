235d0de65ce19541a59ec828a57171a5a9d9fdba
DANIEL VINICIUS RIOS SISMER <daniel_sismer@estudante.sesisenai.org.br>
2026-04-24 20:30:53 -0300

    docs: add project readme and commit strategy

---
+++ sismer/src/main/java/com/driven/event/sismer/SismerApplication.java
@@ -0,0 +1,13 @@
+package com.driven.event.sismer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismerApplication.class, args);
+	}
+
+}
